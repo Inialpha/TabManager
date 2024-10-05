9a09eb51da7efddb3029989ac4b681b45fac28b2
Initech <inimfonebong001@gmail.com>
2024-10-05 16:08:53 +0000

    Add function to get all open windows, save windows

--- src/utils/windows.ts
+++ src/utils/windows.ts
@@ -1,23 +1,81 @@
-export async function getAllWindows(): Promise<chrome.windows.Window[] | []> {
-  let windows: chrome.windows.Window[];
-  const queryObject = {
-    populate: true,
-    windowType: ["normal"]
+
+
+type WindowType = chrome.windows.Window
+type WindiwTab = (chrome.windows.Window & { tabs: chrome.tabs.Tab[] })
+
+
+export async function getOpenWindows(): Promise<(WindiwTab)[]> {
+  try {
+    let windows: WindowType[]  = await chrome.windows.getAll({ populate: false,
+      windowTypes: ['normal', 'popup', 'panel']
+    });
+    const windowsWithTabs: WindiwTab[] = await Promise.all(windows.map(async (window) => {
+      const tabs = await chrome.tabs.query({ windowId: window.id });
+
+      return {
+        ...window,
+        tabs,
+      };
+    }));
+    return windowsWithTabs;
+
+  } catch (error) {
+	  alert("Error retrieving windows and tabs:");
+    alert(error);
+    return [];
   }
-  try {                                         windows = await chrome.windows.getAll(queryObject);
-  } catch (error) {                             console.log(error)
-    windows = []
+}
+
+
+/**
+ * getOpenWinById - Get a window by id
+ *   @id: number window id
+ */
+
+export async function getOpenWinById(id: number): Promise<WindowType | undefined> {
+  let win: WindowType | undefined;
+
+  try {
+    win = await chrome.windows.get(id);
+  } catch (error) {
+    console.log(error)
+    return undefined;
+  }
+  return win;
+}
+
+
+export async function saveWindow(windowId: number) {
+  let win: WindowType;
+  try {
+    win = await chrome.windows.get(windowId);
+    const windows: Record<number, WindowType> = await getSavedWindows();
+    if (win.id !== undefined) {
+      windows[win.id] = win;
+      await chrome.storage.sync.set({windows})
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getSavedWindows(): Promise<Record<number, WindowType>> {
+
+  let windows: Record<number, WindowType>;
+  let storage: any;
+  try {
+    storage = await chrome.storage.sync.get("windows");
+    windows = storage.windows || {};
+  } catch (error) {
+    console.log(error);
+    return {};
   }
   return windows;
 }
 
-export async function getTabByWindow(windowId: number): Promise<chrome.tabs.Tab[] | []> {
-  let tabs: chrome.tabs.Tab[];
-  try {
-    tabs = await chrome.tabs.query({ windowId });
-  } catch (error) {
-    console.log(error)
-    return []
-  }
-  return tabs
+export async function getSavedWinById(windowId: number): Promise<WindowType> {
+  let windows: Record<number, WindowType> = await getSavedWindows();
+
+  return windows[windowId];
 }
+
