f9c46f444df30590d2e645fec31fef6900a4e76a
Initech <inimfonebong001@gmail.com>
2024-10-08 17:14:33 +0000

    Corrected events handler

--- src/background/background.js
+++ src/background/background.js
@@ -17,27 +17,12 @@ const openSettings = () => {
 }
 
 
-chrome.tabs.onCreated.addListener(async (tab) => {
-
-    await chrome.tabs.sendMessage(tab.id, { message: `Tab ${tab.id} created.....` });
-  tabOnCreated(tab);
-
-    chrome.notifications.create({
-      type: "basic",
-      title: "New Tab Created",
-      iconUrl: "",
-      message: `Tab ${tab.id} created.,..!`,
-      priority: 1
-    });
-});
+chrome.tabs.onCreated.addListener(tabOnCreated)
 
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
-});
 
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  chrome.tabs.sendMessage(tab.id, { message: `Tab ${tab.id} createdxxxxx` });
+chrome.tabs.onUpdated.addListener(tabOnUpdated);
+/*  chrome.tabs.sendMessage(tab.id, { message: `Tab ${tab.id} createdxxxxx` });
   tabOnUpdated(tab);
 	chrome.notifications.create({
       type: "basic",
@@ -47,9 +32,8 @@ message: `Tab ${tab.id} created.,..!`,
       priority: 1
     });
 
-});
+});*/
 
 // Attaching the callbacks to Chrome's event listeners
-chrome.tabs.onUpdated.addListener(tabOnUpdated);
 chrome.tabs.onActivated.addListener(tabOnActivated);
 chrome.tabs.onRemoved.addListener(tabOnRemoved);
